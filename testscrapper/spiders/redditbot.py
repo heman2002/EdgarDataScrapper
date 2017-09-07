a78936f13488b68add345d9e9fdf84442a611231
Himanshu Singhal <himanshu.singhal@rocketmail.com>
2017-09-07 23:31:27 +0530

    final scraping

--- testscrapper/spiders/redditbot.py
+++ testscrapper/spiders/redditbot.py
@@ -18,14 +18,19 @@ class RedditbotSpider(scrapy.Spider):
         # pass
         tables = response.xpath('//table')
         for index, tr in enumerate(tables.xpath('.//tr')):
-            for td in tr.xpath('.//td'):
-                for p in td.xpath('.//p//text()').re(r'\bCredit Default Swap\b'):
-                    temp = tables.xpath('.//tr//td//p')[index + 1].extract()
-                    print temp
-                    # scraped_info = {
-                    # 'url' : response.url,
-                    # 'CDS Text' : temp[1],
-                    # 'Expiration Date' : temp[2],
-                    # 'Notional Amount' : temp[3],
-                    # }
-                    # yield scraped_info
+            for td in tr.xpath('.//td//p//text()').re(r'\bCredit Default Swap\b|\bCDS\b|\bCredit Swap\b|\bCredit Default Swaps\b'):
+                temptr = tables.xpath('.//tr')[index + 1]
+                temptd = temptr.xpath('.//td//p//text()').extract()
+                temptd = list(filter(lambda x: x != '\n', temptd))
+                temptd = map(lambda each:each.replace('\t', ''), temptd)
+                scraped_info = {
+                'url' : response.url,
+                'CDS Text' : temptd[0],
+                'Expiration Date' : temptd[1],
+                'Notional Amount' : temptd[2],
+                'Unrealised Appreciation' : temptd[3]
+                }
+                yield scraped_info
+
+# df1 = pandas.read_csv('/home/himanshu/testscrapper/reddit.csv')
+# df1.merge(df, left_on='url', right_on='URL')
